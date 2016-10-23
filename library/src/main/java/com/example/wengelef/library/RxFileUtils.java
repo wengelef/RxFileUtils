59c2d98bd3df033a94a269f294734b81524ace74
Flo <wengelef@gmail.com>
2016-10-23 19:13:16 +0200

    feature (FileUtils): implement File Reader

--- library/src/main/java/com/example/wengelef/library/RxFileUtils.java
+++ library/src/main/java/com/example/wengelef/library/RxFileUtils.java
@@ -1,7 +1,45 @@
 package com.example.wengelef.library;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import rx.Observable;
+import rx.Subscriber;
+
 /**
  * Created by Flo on 23.10.2016.
  */
 public class RxFileUtils {
+
+    @NonNull
+    public static Observable<String> readFileFromAsset(final Context context, final String filename) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                InputStream input = null;
+                try {
+                    input = context.getAssets().open(filename);
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder content = new StringBuilder(input.available());
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        content.append(line);
+                        content.append(System.getProperty("line.separator"));
+                    }
+                    subscriber.onNext(content.toString());
+                    subscriber.onCompleted();
+                } catch (IOException e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
 }
