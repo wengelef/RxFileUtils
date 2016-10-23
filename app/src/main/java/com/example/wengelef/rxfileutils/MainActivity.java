cd547e0a3585d272c2f477ec10c493fc781d7528
Flo <wengelef@gmail.com>
2016-10-23 19:13:48 +0200

    chore (MainActivity): test readFileFromAsset

--- app/src/main/java/com/example/wengelef/rxfileutils/MainActivity.java
+++ app/src/main/java/com/example/wengelef/rxfileutils/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.wengelef.rxfileutils;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.wengelef.library.RxFileUtils;
+
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RxFileUtils.readFileFromAsset(this, "hello_world.txt")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.computation())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d("MainActivity", "onCompleted()");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("MainActivity", "onError()", e);
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.d("MainActivity", "onNext(" + s.trim() + ")");
+                    }
+                });
     }
 }
