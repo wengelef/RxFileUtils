eb7281f0826448049d267d314009b6f2bfe66c85
Flo <wengelef@gmail.com>
2016-10-23 23:49:02 +0200

    feature (RW internal): adds utility Methods to read and write internal storage

--- library/src/main/java/com/wengelef/RxFileUtils.java
+++ library/src/main/java/com/wengelef/RxFileUtils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -39,5 +41,50 @@ public class RxFileUtils {
                 }
             }
         });
+    }
+
+    public static Observable<String> readInternal(final Context context, final String filename) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                FileInputStream fis = null;
+                try {
+                    fis = context.openFileInput(filename);
+                    InputStreamReader isr = new InputStreamReader(fis);
+                    BufferedReader bufferedReader = new BufferedReader(isr);
+                    StringBuilder content = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        content.append(line);
+                    }
+                    subscriber.onNext(content.toString());
+                    subscriber.onCompleted();
+                } catch (IOException e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
+
+    public static Observable<Void> writeInternal(final Context context, final String filename, final String content) {
+        return Observable.create(new Observable.OnSubscribe<Void>() {
+            @Override
+            public void call(Subscriber<? super Void> subscriber) {
+                FileOutputStream outputStream;
+                try {
+                    outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
+                    outputStream.write(content.getBytes());
+                    outputStream.close();
+                    subscriber.onNext(null);
+                    subscriber.onCompleted();
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
+
+    public static Observable writeExternal(final Context context, final String content) {
+        return Observable.empty();
     }
 }
