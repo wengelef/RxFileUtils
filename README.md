6b6c17dbc960277787f59a7e839bab72a524d64d
Florian Wengelewski <florian.wengelewski@rockabyte.com>
2016-10-24 09:52:22 +0200

    chore : docs

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# RxFileUtils
+# RxFileUtils (Android)
+
+Collection of Utility for the Android File System
+
+## Usage
+
+##### Read File from `assets/` Folder:
+
+```java
+        RxFileUtils.readFileFromAsset(this, "hello_world.txt")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.computation())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+                        Timber.d("onCompleted()");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "onError()");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        //Timber.i("onNext(%s)", s.trim());
+                        textView.setText(s);
+                    }
+                });
+
+```
+
+##### Write to Internal storage:
+
+```java
+RxFileUtils.writeInternal(this, "hello_test.txt", "test test test")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<Void>() {
+                    @Override
+                    public void onCompleted() {
+                        Timber.i("Write internal complete");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "onError");
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        Timber.i("Write internal complete");
+                    }
+                });
+```
+
+##### Read from Internal storage:
+
+```java
+RxFileUtils.readInternal(this, "hello_test.txt")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+                        Timber.i("onCompleted");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "onError");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Timber.i("Internal File onNext(%s)", s);
+                    }
+                });
+```
+
+
+## License
+
+Copyright 2016 Florian Wengelewski
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- library/src/main/java/com/wengelef/RxFileUtils.java
+++ library/src/main/java/com/wengelef/RxFileUtils.java
@@ -1,10 +1,28 @@
+/*
+ * Copyright (c) wengelef 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.wengelef;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,71 +32,110 @@ import rx.Observable;
 import rx.Subscriber;
 
 /**
- * Created by Flo on 23.10.2016.
+ * Collection of Utility for the Android File System
  */
 public class RxFileUtils {
 
+    /**
+     * Read file from your main/assets Directory
+     *
+     * @param context  Context, not null
+     * @param filename  Filename of the File in assets Directory
+     * @return <code>Observable<String></code> that gets the File content emitted as <code>String</code> in <code>onNext()</code>.
+     *         <code>onError()</code> is emitted if the given Context is null or File operation meets an <code>IOException</code>
+     */
     @NonNull
-    public static Observable<String> readFileFromAsset(final Context context, final String filename) {
+    public static Observable<String> readFileFromAsset(@Nullable final Context context, final String filename) {
         return Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
-                InputStream input = null;
-                try {
-                    input = context.getAssets().open(filename);
+                if (context != null) {
+                    InputStream input = null;
+                    try {
+                        input = context.getAssets().open(filename);
 
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
-                    StringBuilder content = new StringBuilder(input.available());
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        content.append(line);
-                        content.append(System.getProperty("line.separator"));
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
+                        StringBuilder content = new StringBuilder(input.available());
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            content.append(line);
+                            content.append(System.getProperty("line.separator"));
+                        }
+                        subscriber.onNext(content.toString());
+                        subscriber.onCompleted();
+                    } catch (IOException e) {
+                        subscriber.onError(e);
                     }
-                    subscriber.onNext(content.toString());
-                    subscriber.onCompleted();
-                } catch (IOException e) {
-                    subscriber.onError(e);
+                } else {
+                    subscriber.onError(new IllegalArgumentException("Context must not be null"));
                 }
             }
         });
     }
 
-    public static Observable<String> readInternal(final Context context, final String filename) {
+    /**
+     * Reads a File from internal Storage
+     *
+     * @param context  Context, not null
+     * @param filename  Filename of the File to read from Internal Storage
+     * @return <code>Observable<String></code> that gets the File content emitted as <code>String</code> in <code>onNext()</code>.
+     *         <code>onError()</code> is emitted if the given Context is null or File operation meets an <code>IOException</code>
+     */
+    @NonNull
+    public static Observable<String> readInternal(@Nullable final Context context, final String filename) {
         return Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
-                FileInputStream fis = null;
-                try {
-                    fis = context.openFileInput(filename);
-                    InputStreamReader isr = new InputStreamReader(fis);
-                    BufferedReader bufferedReader = new BufferedReader(isr);
-                    StringBuilder content = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        content.append(line);
+                if (context != null) {
+                    FileInputStream fis = null;
+                    try {
+                        fis = context.openFileInput(filename);
+                        InputStreamReader isr = new InputStreamReader(fis);
+                        BufferedReader bufferedReader = new BufferedReader(isr);
+                        StringBuilder content = new StringBuilder();
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            content.append(line);
+                        }
+                        subscriber.onNext(content.toString());
+                        subscriber.onCompleted();
+                    } catch (IOException e) {
+                        subscriber.onError(e);
                     }
-                    subscriber.onNext(content.toString());
-                    subscriber.onCompleted();
-                } catch (IOException e) {
-                    subscriber.onError(e);
+                } else {
+                    subscriber.onError(new IllegalArgumentException("Context must not be null"));
                 }
             }
         });
     }
 
-    public static Observable<Void> writeInternal(final Context context, final String filename, final String content) {
+    /**
+     * Write Content to a File in internal Storage
+     *
+     * @param context  Context, not null
+     * @param filename  Filename of the File that will be saved
+     * @param content  Content to write into the File
+     * @return <code>Observable<Void></code> that emits <code>onNext()</code> and <code>onCompleted()</code> when the File is saved.
+     *         <code>onError()</code> is emitted if the given Context is null or File operation meets an <code>IOException</code>
+     */
+    @NonNull
+    public static Observable<Void> writeInternal(@Nullable final Context context, final String filename, final String content) {
         return Observable.create(new Observable.OnSubscribe<Void>() {
             @Override
             public void call(Subscriber<? super Void> subscriber) {
-                FileOutputStream outputStream = null;
-                try {
-                    outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
-                    outputStream.write(content.getBytes());
-                    outputStream.close();
-                    subscriber.onNext(null);
-                    subscriber.onCompleted();
-                } catch (Exception e) {
-                    subscriber.onError(e);
+                if (context != null) {
+                    FileOutputStream outputStream = null;
+                    try {
+                        outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
+                        outputStream.write(content.getBytes());
+                        outputStream.close();
+                        subscriber.onNext(null);
+                        subscriber.onCompleted();
+                    } catch (IOException e) {
+                        subscriber.onError(e);
+                    }
+                } else {
+                    subscriber.onError(new IllegalArgumentException("Context must not be null"));
                 }
             }
         });
